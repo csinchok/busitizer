efd515d5704b1417a738cd9e5f5550b4a1bc4dba
Sam Johnson <sjohns52@gmail.com>
2012-08-18 15:27:55 -0500

    changed email password

--- busitizer/settings.py
+++ busitizer/settings.py
@@ -114,7 +114,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'busitizer@gmail.com'
-EMAIL_HOST_PASSWORD = 'busitize'
+
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
