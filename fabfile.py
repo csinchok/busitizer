69bf428738efa17ffba1e4c2b3e781ccfe298f10
Chris Sinchok <csinchok@theonion.com>
2012-08-18 04:12:19 -0500

    Fixing gitignore

--- fabfile.py
+++ fabfile.py
@@ -4,13 +4,23 @@ from fabric.api import *
 from fabric.contrib.project import rsync_project
 from fabric.contrib import files
 
+from contextlib import contextmanager as _contextmanager
+
 env.hosts = ['66.175.213.211']
 env.user = 'fabric'
 env.webroot = '/www/'
+env.projectroot = os.path.join(env.webroot, 'busitizer')
+env.activate = 'source .env/bin/activate'
+
+@_contextmanager
+def virtualenv():
+    with cd(env.projectroot):
+        with prefix(env.activate):
+            yield
 
 def deploy():
     rsync_project(env.webroot, delete=True, exclude=['.env', '*.pyc', '.git'])
-    with cd(os.path.join(env.webroot, 'busitizer')):
+    with virtualenv():
         if not files.exists('.env'):
             run('virtualenv .env')
         run('pip install -r requirements.txt')
