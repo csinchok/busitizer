221931c3db962d56aed3279bd1fefe41d625866e
Chris Sinchok <csinchok@theonion.com>
2012-08-18 06:24:09 -0500

    Static stuff

--- busitizer/production.py
+++ busitizer/production.py
@@ -18,3 +18,10 @@ DATABASES = {
         'HOST': pgpass[0],
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
