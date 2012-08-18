01f7283dfeb04fdfc7d334e45ab25ee956447826
Chris Sinchok <csinchok@theonion.com>
2012-08-18 05:15:59 -0500

    I'm bad at gitignore?

---
+++ busitizer/production.py
@@ -0,0 +1,4 @@
+from busitizer.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
--- busitizer/settings.py
+++ busitizer/settings.py
@@ -1,4 +1,5 @@
-# Django settings for busitizer project.
+import os
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -56,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'webroot', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
